0dcb13b830aee3b98268857c9c7635ab9239ac7a
Yash-Gupta03 <yashgupta.fast@gmail.com>
2023-04-19 21:28:47 +0530

    sign up page created

---
+++ frontend/script.js
@@ -0,0 +1,26 @@
+// Function invoked after the form is submittedform
+function savedata(e) {
+  e.preventDefault();
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  // Making values in the form null after submitting the form
+  document.getElementById("name").value = null;
+  document.getElementById("email").value = null;
+  document.getElementById("password").value = null;
+
+  // Creating object of the data
+  const obj = {
+    name: name,
+    email: email,
+    password: password,
+  };
+  // Post request to the server to store user details
+  axios
+    .post("http://localhost:3000/user/sign-up", obj)
+    .then((response) => {
+      const newItem = response.data.newItemDetail;
+    })
+    .catch((err) => console.log(err));
+}
