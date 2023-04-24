ed5213068b99cf473f9a4bc75c105a79c45980d7
Yash-Gupta03 <yashgupta.fast@gmail.com>
2023-04-24 18:34:47 +0530

    forget password frontend created

---
+++ frontend/forgetPassword.js
@@ -0,0 +1,6 @@
+async function forgetpassworddata(event){
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+
+    const res = await axios.post('http://localhost:3000/password/forgotpassword', {email});
+}
