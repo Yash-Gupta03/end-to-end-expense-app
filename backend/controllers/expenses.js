35a7aad2b72f828536df8620b1a2a70f77aacb17
Yash-Gupta03 <yashgupta.fast@gmail.com>
2023-04-24 17:45:36 +0530

    transactions added

--- backend/controllers/expenses.js
+++ backend/controllers/expenses.js
@@ -1,5 +1,6 @@
 const Expense = require("../models/expense");
 const User = require('../models/user');
+const sequelize = require("../utils/database");
 
 function isStringInvalid(string) {
   if (string == undefined || string.length === 0) {
@@ -10,6 +11,7 @@ function isStringInvalid(string) {
 }
 
 exports.addExpense = async (req, res, next) => {
+  const t = await sequelize.transaction();
   const { price, description, category } = req.body;
   if (
     isStringInvalid(price) ||
@@ -18,19 +20,21 @@ exports.addExpense = async (req, res, next) => {
   ) {
     res.status(400).json({ message: "bad parameters" });
   }
+  try{
   const total = Number(req.user.totalExpense) + Number(price);
-  User.update({totalExpense:total}, {where:{id:req.user.id}})
-
+  const userupdate = await User.update({totalExpense:total}, {where:{id:req.user.id}}, {transaction:t});
   const data = await Expense.create({
     price: price,
     description: description,
     category: category,
     userId: req.user.id,
-    
-
-  });
+  }, {transaction:t});
+  await t.commit();
   res.json({ newExpenseDetail: data });
-};
+}catch(err){
+  await t.rollback();
+  console.log(err);
+}};
 
 exports.getExpense = async (req, res, next) => {
   const data = await Expense.findAll({ where: { userId: req.user.id } });
@@ -38,8 +42,19 @@ exports.getExpense = async (req, res, next) => {
 };
 
 exports.deleteExpense = async (req, res, next) => {
-  const data = await Expense.destroy({
-    where: { id: req.params.id, userId: req.user.id },
-  });
-  res.status(200).json({ data: data });
-};
+  
+    const t = await sequelize.transaction();
+    try{
+    const price = await Expense.findAll({where:{id:req.params.id}, attributes:['price']},{transaction:t});
+    console.log(`price = `, price);
+    const total = Number(req.user.totalExpense) - Number(price[0].price);
+    const userupdate = await User.update({totalExpense:total}, {where:{id:req.user.id}}, {transaction:t});
+    const data = await Expense.destroy({
+      where: { id: req.params.id, userId: req.user.id },
+    });
+    await t.commit();
+    res.status(200).json({ data: data });
+  }catch(error){
+    await t.rollback();
+  }
+}
--- backend/controllers/premium.js
+++ backend/controllers/premium.js
@@ -18,8 +18,8 @@ order:[[sequelize.col('totalExpense'), 'DESC']]
 
     
 console.log(leaderboardDetails);
-leaderboardDetails.sort((a, b) => b.total_cost - a.total_cost)
+res.status(202).json({ expenseLeaderboard: leaderboardDetails});
+// leaderboardDetails.sort((a, b) => b.total_cost - a.total_cost)
 
-    res.status(202).json({ expenseLeaderboard: leaderboardDetails});
   };
 
